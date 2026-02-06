7826c9ac944b6caaa862ffd82e3ae93d1b7d73ad
lfarssi <medlfarssi10@gmail.com>
2026-02-06 18:53:49 +0100

    http models

--- src/HttpModels.java
+++ src/HttpModels.java
@@ -1,4 +1,15 @@
+import java.util.*;
+
 public class HttpModels {
-    
+    public static final class Request{
+        public String method;
+        public String target;
+        public String path;
+        public String query;
+        public String version;
+
+        public Map<String, String> headers=new HashMap<>();
+        public byte[] body= new byte[0];
+    }
 }
  
